9b89bafd969a946512cee1dfe7dd44df8fc3d9a8
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-09-18 18:32:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadMeal></LoadMeal>
+    </div>
+  );
+}
+
+function LoadMeal() {
+  const [food, setFood] = useState([]);
+  useEffect(() => {
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then(res => res.json())
+      .then(data => setFood(data.categories))
+  }, [])
+  return (
+    <div>
+      {
+        food.map(foods => <MealItem foodName={foods.strCategory} foodDesc={foods.strCategoryDescription} foodImg={foods.strCategoryThumb}></MealItem>)
+      }
+    </div>
+  );
+}
+
+function MealItem(props) {
+  console.log(props);
+  return (
+    <div>
+      <div>
+        <img src={props.foodImg} alt="" />
+        <h3>Food Name : {props.foodName}</h3>
+        <p>{props.foodDesc}</p>
+      </div>
     </div>
   );
 }
